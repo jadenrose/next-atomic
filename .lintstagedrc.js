3392dd132b946c67b5ccd2ede886c86c562a17fe
Jaden Rose <jadenrrose@gmail.com>
2024-01-03 19:14:38 -0700

    fix: adds ignore pattern to lint-staged

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,9 @@
 import path from 'path'
 
+const ignorePatterns = {
+  md: ['plop-templates'],
+}
+
 const buildEslintCommand = (filenames) =>
   `next lint --file ${filenames
     .map((f) => path.relative(process.cwd(), f))
@@ -8,10 +12,17 @@ const buildEslintCommand = (filenames) =>
 const lintStagedConfig = {
   '*.{js,jsx,ts,tsx}': [buildEslintCommand],
   '*.md': (filenames) =>
-    filenames.map((filename) => {
-      console.log(filename)
-      return `npx remark ${filename}`
-    }),
+    filenames
+      .filter((filename) => {
+        let ignored = false
+        for (const pattern of ignorePatterns.md)
+          if (filename.includes(pattern)) ignored = true
+        return !ignored
+      })
+      .map((filename) => {
+        console.log('Linting Markdown File:', filename)
+        return `npx remark ${filename}`
+      }),
 }
 
 export default lintStagedConfig
